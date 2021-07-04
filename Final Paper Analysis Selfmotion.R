00aab4fe0ed9632f3ece47aab00610a648df32c5
b_jorges <bjoern_joerges@hotmail.de>
2021-07-04 01:16:41 -0400

    remove Bayesian analysis

--- Final Paper Analysis Selfmotion.R
+++ Final Paper Analysis Selfmotion.R
@@ -3,18 +3,24 @@ require(lme4) #package for statistical analysis
 setwd(dirname(rstudioapi::getSourceEditorContext()$path)) #set path of this script as working directory
 source("Final Paper Data Preprocessing.r")
 
-
 #########regular results
-mod1_Regular_Include = glmer(cbind(Yes, Total - Yes) ~ Include*Congruent*velH_Pest + (velH_Pest + Congruent  | Participant) + (velH_Pest + Congruent | velH),
-                             family = binomial(link = "probit"),
-                             data = Data_GLM,
-                             nAGQ = 0,
-                             glmerControl(optimizer = "nloptwrap"))
+# Include = glmer(cbind(Yes, Total - Yes) ~ Include*Congruent*velH_Pest + (velH_Pest + Congruent  | Participant) + (velH_Pest + Congruent | velH),
+#                              family = binomial(link = "probit"),
+#                              data = Data_GLM,
+#                              nAGQ = 0,
+#                              glmerControl(optimizer = "nloptwrap"))
+# 
+# Include_Null = glmer(cbind(Yes, Total - Yes) ~ Congruent*velH_Pest + (velH_Pest + Congruent  | Participant) + (velH_Pest + Congruent | velH),
+#                                   family = binomial(link = "probit"),
+#                                   data = Data_GLM,
+#                                   nAGQ = 0,
+#                                   glmerControl(optimizer = "nloptwrap"))
+# 
+# save(Include, file = paste0(dirname(rstudioapi::getSourceEditorContext()$path),
+#                                                               "/SavedVariables/Include.RData"))
+# save(Include_Null, file = paste0(dirname(rstudioapi::getSourceEditorContext()$path),
+#                                                               "/SavedVariables/Include_Null.RData"))
+load(file=paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/SavedVariables/Include.RData"))
+load(file=paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/SavedVariables/Include_Null.RData"))
 
-mod1_Regular_Include_Null = glmer(cbind(Yes, Total - Yes) ~ Congruent*velH_Pest + (velH_Pest + Congruent  | Participant) + (velH_Pest + Congruent | velH),
-                                  family = binomial(link = "probit"),
-                                  data = Data_GLM,
-                                  nAGQ = 0,
-                                  glmerControl(optimizer = "nloptwrap"))
-
-anova(mod1_Regular_Include, mod1_Regular_Include_Null)
+anova(Include, Include_Null)
